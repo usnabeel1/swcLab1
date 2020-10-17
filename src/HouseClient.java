8a563b4fb8da74c6e6a1a96b85f0086516814a15
Benedicta <nnennaanih1@gmail.com>
2020-10-17 09:26:02 +0300

    Delete some comments and change one class name

---
+++ src/HouseClient.java
@@ -0,0 +1,17 @@
+
+public class HouseClient {
+
+    public static void main(String[] args) {
+
+        HouseTemplate houseType = new WoodenHouse();
+
+        //using template method
+        houseType.buildHouse();
+        System.out.println("************");
+
+        houseType = new GlassHouse();
+
+        houseType.buildHouse();
+    }
+
+}
